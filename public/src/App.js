01a58c9ea38d548fb44bedf377ca967d76f33317
Tushar <tusharsankhla.10111824@gmail.com>
2025-04-06 13:12:34 +0530

    first commit

---
+++ public/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello from React!</h1>
+      <p>This is a simple app to deploy on Azure via Jenkins.</p>
+    </div>
+  );
+}
+
+export default App;
